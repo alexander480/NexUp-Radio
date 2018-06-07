a3872ace5b240a4ccc6c993ca50389ddb7f4763c
Alexander Lester <alexander480@gmail.com>
2018-06-06 21:42:02 -0400

    added pod files

---
+++ Pods/Target Support Files/Pods-NexUp/Pods-NexUp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NexUp : NSObject
+@end
+@implementation PodsDummy_Pods_NexUp
+@end
--- Pods/Target Support Files/Pods-TheLocalPlug/Pods-TheLocalPlug-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TheLocalPlug : NSObject
-@end
-@implementation PodsDummy_Pods_TheLocalPlug
-@end
