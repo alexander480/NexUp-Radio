b5a3f4a70cbb5714b0c56096ecf706779053003d
alexander480 <alexander480@gmail.com>
2018-08-14 04:19:30 -0400

    - Added PDF WebViews

--- NexUp/PrivacyVC.swift
+++ NexUp/PrivacyVC.swift
@@ -7,10 +7,37 @@
 //
 
 import Foundation
+import WebKit
 import UIKit
 
-class PrivacyVC: UIViewController {
+class PrivacyVC: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    var doneButton: UIButton!
+    
+    override func loadView() {
+        super.loadView()
+        
+        let webViewFrame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height - 50.0)
+        self.webView = WKWebView(frame: webViewFrame, configuration: WKWebViewConfiguration())
+        self.webView.uiDelegate = self
+        
+        let doneButtonFrame = CGRect(x: 0.0, y: self.view.frame.size.height - 50.0, width: self.view.frame.size.width, height: 50.0)
+        self.doneButton = UIButton(frame: doneButtonFrame)
+        self.doneButton.setTitle("Done", for: .normal)
+        self.doneButton.addTarget(self, action: Selector(("doneAction")), for: .touchUpInside)
+        
+        
+        self.view.addSubview(self.webView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let url = URL(string: "https://designsbylagb.com/nexup-terms.html") {
+            self.webView.load(URLRequest(url: url))
+        }
+    }
+    
+    private func doneAction(sender: UIButton!) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
--- NexUp/TermsVC.swift
+++ NexUp/TermsVC.swift
@@ -7,10 +7,24 @@
 //
 
 import Foundation
+import WebKit
 import UIKit
 
-class TermsVC: UIViewController {
+class TermsVC: UIViewController, WKUIDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        let webViewFrame = CGRect(x: 0.0, y: 0.0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        self.webView = WKWebView(frame: webViewFrame, configuration: WKWebViewConfiguration())
+        self.webView.uiDelegate = self
+        self.view.addSubview(self.webView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let url = URL(string: "https://designsbylagb.com/nexup-terms.html") {
+            self.webView.load(URLRequest(url: url))
+        }
     }
 }
