5baf693faefe6097944ece5440994c4f66ea076b
alexander480 <alexander480@gmail.com>
2018-12-04 16:05:27 -0500

    changed some shit to reflect prototype

--- NexUp/StoryboardExt.swift
+++ NexUp/StoryboardExt.swift
@@ -15,9 +15,53 @@ import UIKit
     @IBInspectable var shadowOpacity: Float = 1.0 { didSet { layer.shadowOpacity = shadowOpacity } }
     @IBInspectable var shadowOffset: CGSize = CGSize.zero { didSet { layer.shadowOffset = shadowOffset } }
     @IBInspectable var shadowRadius: CGFloat = 10 { didSet { layer.shadowRadius = shadowRadius } }
+    @IBInspectable var borderWidth: CGFloat { get { return layer.borderWidth } set { layer.borderWidth = newValue } }
+    @IBInspectable var borderColor: UIColor? {
+        get { if let color = layer.borderColor { return UIColor(cgColor: color) }; return nil }
+        set { if let color = newValue { layer.borderColor = color.cgColor } else { layer.borderColor = nil } }
+    }
 }
 
 @IBDesignable class TableClass: UITableView { @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius } } }
-@IBDesignable class ButtonClass: UIButton { @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius  } } }
-@IBDesignable class TextFieldClass: UIButton { @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius  } } }
-@IBDesignable class ImageViewClass: UIImageView { @IBInspectable var cornerRadius: CGFloat = CGFloat.init(exactly: NSNumber(value: 1))! { didSet { layer.cornerRadius = cornerRadius } } }
+
+@IBDesignable class ButtonClass: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius  } }
+    @IBInspectable var shadowColor: CGColor = UIColor.black.cgColor { didSet { layer.shadowColor = shadowColor } }
+    @IBInspectable var shadowOpacity: Float = 1.0 { didSet { layer.shadowOpacity = shadowOpacity } }
+    @IBInspectable var shadowOffset: CGSize = CGSize.zero { didSet { layer.shadowOffset = shadowOffset } }
+    @IBInspectable var shadowRadius: CGFloat = 10 { didSet { layer.shadowRadius = shadowRadius } }
+    @IBInspectable var borderWidth: CGFloat { get { return layer.borderWidth } set { layer.borderWidth = newValue } }
+    @IBInspectable var borderColor: UIColor? {
+        get { if let color = layer.borderColor { return UIColor(cgColor: color) }; return nil }
+        set { if let color = newValue { layer.borderColor = color.cgColor } else { layer.borderColor = nil } }
+    }
+}
+
+
+@IBDesignable class TextFieldClass: UIButton {
+    @IBInspectable var cornerRadius: CGFloat = 0 { didSet { layer.cornerRadius = cornerRadius  } }
+    @IBInspectable var shadowColor: CGColor = UIColor.black.cgColor { didSet { layer.shadowColor = shadowColor } }
+    @IBInspectable var shadowOpacity: Float = 1.0 { didSet { layer.shadowOpacity = shadowOpacity } }
+    @IBInspectable var shadowOffset: CGSize = CGSize.zero { didSet { layer.shadowOffset = shadowOffset } }
+    @IBInspectable var shadowRadius: CGFloat = 10 { didSet { layer.shadowRadius = shadowRadius } }
+    @IBInspectable var borderWidth: CGFloat { get { return layer.borderWidth } set { layer.borderWidth = newValue } }
+    @IBInspectable var borderColor: UIColor? {
+        get { if let color = layer.borderColor { return UIColor(cgColor: color) }; return nil }
+        set { if let color = newValue { layer.borderColor = color.cgColor } else { layer.borderColor = nil } }
+    }
+}
+
+@IBDesignable class ImageViewClass: UIImageView {
+    @IBInspectable var cornerRadius: CGFloat = CGFloat.init(exactly: NSNumber(value: 1))! { didSet { layer.cornerRadius = cornerRadius } }
+    @IBInspectable var shadowColor: CGColor = UIColor.black.cgColor { didSet { layer.shadowColor = shadowColor } }
+    @IBInspectable var shadowOpacity: Float = 1.0 { didSet { layer.shadowOpacity = shadowOpacity } }
+    @IBInspectable var shadowOffset: CGSize = CGSize.zero { didSet { layer.shadowOffset = shadowOffset } }
+    @IBInspectable var shadowRadius: CGFloat = 10 { didSet { layer.shadowRadius = shadowRadius } }
+    @IBInspectable var borderWidth: CGFloat { get { return layer.borderWidth } set { layer.borderWidth = newValue } }
+    @IBInspectable var borderColor: UIColor? {
+        get { if let color = layer.borderColor { return UIColor(cgColor: color) }; return nil }
+        set { if let color = newValue { layer.borderColor = color.cgColor } else { layer.borderColor = nil } }
+    }
+}
+
+
